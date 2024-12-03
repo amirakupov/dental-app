b570e50832ec59284cccff104d29efe96073a17b
amirkukei <amirakupov@gmail.com>
2024-12-03 17:47:48 +0100

    correct links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ const Footer = () => {
           <div className="social-media">
             <FaInstagram className="icon" />
             <a
-              href="https://www.instagram.com"
+              href="https://www.instagram.com/dr_tatjana_ladstaetter?igsh=azV5dHNsazhnaDdj"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -32,7 +32,7 @@ const Footer = () => {
         <div className="footer-map">
           <iframe
             title="Google Maps"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2651.755407271781!2d16.467400415889056!3d48.1446788792236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d055c9f7eb65b%3A0x546eef31b2286ff5!2sBrauhausstra%C3%9Fe%2023%2C%202320%20Schwechat%2C%20Austria!5e0!3m2!1sen!2sus!4v1698319463204!5m2!1sen!2sus"
+            src="https://www.google.com/maps/place/Zahn%C3%A4rztin+Ordination:+Dr.med.dent.+Tatjana+Ladst%C3%A4tter+Alle+Kassen+und+Privat/@48.1437361,16.4728395,17z/data=!3m1!5s0x476daae5b1a85ddd:0xbb508064e8a5de7d!4m6!3m5!1s0x476dab4e53b1b16f:0x55747ae5bc5480e4!8m2!3d48.1422658!4d16.4736967!16s%2Fg%2F11vqrs32yy?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D"
             width="100%"
             height="250"
             style={{ border: "0" }}
