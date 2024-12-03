bd81f31c62b1f1fb813e10db7842f6e826e78365
amirkukei <amirakupov@gmail.com>
2024-12-03 17:55:05 +0100

    correct links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,7 +32,7 @@ const Footer = () => {
         <div className="footer-map">
           <iframe
             title="Google Maps"
-            src="https://www.google.com/maps/place/Zahn%C3%A4rztin+Ordination:+Dr.med.dent.+Tatjana+Ladst%C3%A4tter+Alle+Kassen+und+Privat/@48.1437361,16.4728395,17z/data=!3m1!5s0x476daae5b1a85ddd:0xbb508064e8a5de7d!4m6!3m5!1s0x476dab4e53b1b16f:0x55747ae5bc5480e4!8m2!3d48.1422658!4d16.4736967!16s%2Fg%2F11vqrs32yy?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D"
+            src="https://maps.app.goo.gl/8oGWrHysP8vd6kkK6"
             width="100%"
             height="250"
             style={{ border: "0" }}
