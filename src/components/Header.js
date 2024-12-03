62113e80bbdb27e174e6d03d820bfb2e7114525a
amirkukei <amirakupov@gmail.com>
2024-12-03 14:30:40 +0100

    burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
 import { Link } from 'react-scroll';
-import './Header.css';
 import { FaPhone, FaBars, FaTimes } from 'react-icons/fa'; // Import hamburger and close icons
 import { IoMdArrowDropdown } from 'react-icons/io';
+import './Header.css';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false); // Toggle for hamburger menu
   const [languageOpen, setLanguageOpen] = useState(false); // Toggle for language dropdown
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  
+    if (!menuOpen) {
+      document.body.classList.add('mobile-menu-open');
+    } else {
+      document.body.classList.remove('mobile-menu-open');
+    }
+  };
+  
+
   return (
     <header className="header">
       <nav className="nav">
+        <div className="burger-menu" onClick={toggleMenu}>
+          {menuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </div>
 
-        {/* Navigation List */}
         <ul className={`nav-list ${menuOpen ? 'open' : ''}`}>
           <li>
             <Link to="HOME" smooth={true} duration={500} onClick={() => setMenuOpen(false)}>
@@ -41,19 +54,12 @@ const Header = () => {
           </li>
         </ul>
 
-        {/* Actions */}
         <div className="actions">
-          {/* Call Now Button */}
           <a href="tel:+4313671607" className="call-now">
             <FaPhone className="phone-icon" />
             <span>CALL NOW</span>
           </a>
-
-          {/* Language Dropdown */}
-          <div
-            className="language"
-            onClick={() => setLanguageOpen(!languageOpen)}
-          >
+          <div className="language" onClick={() => setLanguageOpen(!languageOpen)}>
             <span>ENG</span>
             <IoMdArrowDropdown className="dropdown-arrow" />
             {languageOpen && (
@@ -65,11 +71,47 @@ const Header = () => {
           </div>
         </div>
       </nav>
+
+      {/* Mobile Menu */}
+      <div className={`mobile-menu ${menuOpen ? 'active' : ''}`}>
+        <ul className="mobile-nav-list">
+          <li>
+            <Link to="HOME" smooth={true} duration={500} onClick={toggleMenu}>
+              HOME
+            </Link>
+          </li>
+          <li>
+            <Link to="ORDINATION" smooth={true} duration={500} onClick={toggleMenu}>
+              ORDINATION
+            </Link>
+          </li>
+          <li>
+            <Link to="TEAM" smooth={true} duration={500} onClick={toggleMenu}>
+              TEAM
+            </Link>
+          </li>
+          <li>
+            <Link to="LEISTUNGEN" smooth={true} duration={500} onClick={toggleMenu}>
+              LEISTUNGEN
+            </Link>
+          </li>
+          <li>
+            <Link to="KONTAKT" smooth={true} duration={500} onClick={toggleMenu}>
+              KONTAKT
+            </Link>
+          </li>
+          <a href="tel:+4313671607" className="call-now">
+            <FaPhone className="phone-icon" />
+            <span>CALL NOW</span>
+          </a>
+        </ul>
+      </div>
     </header>
   );
 };
 
 export default Header;
+
 
 
 
