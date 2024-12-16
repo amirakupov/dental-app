630a05513baf1860b613ce435867a90250a5c790
amirkukei <amirakupov@gmail.com>
2024-12-16 18:40:34 +0300

    address

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { FaPhone, FaMailBulk, FaInstagram } from 'react-icons/fa';
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import './Footer.css';
 
 const Footer = () => {
   const { t } = useTranslation();
-
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyDw7Snl7bhWqaPxBai-lEV2Z5KE2EWPKTE',
-  });
-
-  const mapContainerStyle = {
-    width: '100%',
-    height: '300px',
-    borderRadius: '8px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-  };
-
-  const center = {
-    lat: 48.2082, // Latitude of your location
-    lng: 16.3738, // Longitude of your location
-  };
 
   return (
     <footer className="footer">
@@ -33,38 +16,42 @@ const Footer = () => {
           <p>{t('footer.address.line2')}</p>
           <p>{t('footer.address.country')}</p>
           <div className="contact-info">
-            <FaPhone className="icon" />
-            <a href="tel:+4313671607">{t('footer.contact.phone')}</a>
+            <FaPhone className="icon" aria-label="Phone" />
+            <a href="tel:+4313671607" aria-label="Call us">
+              {t('footer.contact.phone')}
+            </a>
           </div>
           <div className="contact-info">
-            <FaMailBulk className="icon" />
-            <a href="mailto:dr.ladstaetter.tanja@gmail.com">{t('footer.contact.email')}</a>
+            <FaMailBulk className="icon" aria-label="Email" />
+            <a href="mailto:dr.ladstaetter.tanja@gmail.com" aria-label="Email us">
+              {t('footer.contact.email')}
+            </a>
           </div>
           <div className="social-media">
-            <FaInstagram className="icon" />
+            <FaInstagram className="icon" aria-label="Instagram" />
             <a
               href="https://www.instagram.com/dr_tatjana_ladstaetter?igsh=azV5dHNsazhnaDdj"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="Follow us on Instagram"
             >
               {t('footer.social.instagram')}
             </a>
           </div>
         </div>
 
-        {/* Right Section: Google Map */}
+        {/* Right Section: Embedded Google Map */}
         <div className="footer-map">
-          {isLoaded ? (
-            <GoogleMap
-              mapContainerStyle={mapContainerStyle}
-              center={center}
-              zoom={15}
-            >
-              <Marker position={center} />
-            </GoogleMap>
-          ) : (
-            <p>{t('loadingMap')}</p> // Display while the map is loading
-          )}
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11308.21333572552!2d16.4658114!3d48.1422658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476dab4e53b1b16f%3A0x55747ae5bc5480e4!2sZahn%C3%A4rztin%20Ordination%3A%20Dr.med.dent.%20Tatjana%20Ladst%C3%A4tter%20Alle%20Kassen%20und%20Privat!5e0!3m2!1sen!2sat!4v1702547651684!5m2!1sen!2sat"
+            width="100%"
+            height="300"
+            style={{ border: 0, borderRadius: '8px' }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Google Maps Location"
+          ></iframe>
         </div>
       </div>
     </footer>
